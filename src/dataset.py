ba4e5c56293ea0ed232022a2f4ad646d749aa53c
adhiiisetiawan <adhisetiawan518@gmail.com>
2022-05-30 20:29:17 +0700

    get annotation bounding box

--- src/dataset.py
+++ src/dataset.py
@@ -34,3 +34,66 @@ class MicrocontrollerDataset(Dataset):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
         image_resized = cv2.resize(image, (self.width, self.height))
         image_resized /= 255
+
+        # capture the corresponding xml file for getting the annotation
+        annot_filename = image_name[:4] + '.xml'
+        annot_file_path = os.path.join(self.dir_path, annot_filename)
+
+        boxes = []
+        labels = []
+        tree = et.parse(annot_file_path)
+        root = tree.getroot()
+
+        # get the height and width of the image
+        image_width = image.shape[1]
+        image_height = image.shape[0]
+
+        # box coordinates fro xml files are extracted and corrected for image size given
+        for member in root.findall('object'):
+            # map the current object name to `classes` list to get...
+            # ... the label index and append to `labels` list
+            labels.append(self.classes.index(member.find('name').text))
+
+            xmin = int(member.find('bndbox').find('xmin').text)
+            xmax = int(member.find('bndbox').find('xmax').text)
+            ymin = int(member.find('bndbox').find('ymin').text)
+            ymax = int(member.find('bndbox').find('ymax').text)
+
+            xmin_final = (xmin/image_width) * self.width
+            xmax_final = (xmax/image_width) * self.width
+            ymin_final = (ymin/image_height) * self.height
+            ymax_final = (ymax/image_height) * self.height
+
+            boxes.append([xmin_final, ymin_final, xmax_final, ymax_final])
+
+        # bounding box to tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # area of the bounding boxes
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # no crowd instances
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+        # labels to tensor
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        # prepare the final `target` dictionary
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+        image_id = torch.tensor([idx])
+        target["image_id"] = image_id
+
+        # apply the image transforms
+        if self.transforms:
+            sample = self.transforms(image = image_resized,
+                                     bboxes = target['boxes'],
+                                     labels = labels)
+            image_resized = sample['image']
+            target['boxes'] = torch.Tensor(sample['bboxes'])
+            
+        return image_resized, target
+
+    def __len__(self):
+        return len(self.all_images)
+
